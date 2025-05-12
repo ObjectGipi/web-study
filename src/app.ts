5475eb7ebdd6ac9b20e6c43cfba572ca98780052
sjkimwiz <sjkimwiz@gmail.com>
2025-05-12 18:29:27 +0900

    fix

--- src/app.ts
+++ src/app.ts
@@ -7,74 +7,65 @@ const app = async () => {
       `로그인 할 시 1을 누르고, 회원가입을 할 시 2를 눌러주세요`,
     );
     if (answer === "1") {
-      const email = await input(`이메일: `);
+      const email = await input(`이메일을 입력하세요: `);
       if (!email.includes("@")) {
         console.log(`잘못된 입력입니다. 이메일에 @를 포함시켜주세요.`)
         continue;
       }
 
-      const password = await input(`비밀번호: `);
+      const password = await input(`비밀번호를 입력하세요: `);
       if (password.length < 4) {
         console.log(`잘못된 입력입니다. 비밀번호는 4자리이상 입력해주세요.`);
         continue;
       }
 
-      const usersTxt = fs.readFileSync("users.txt").toString();
-      const usersArray = usersTxt.split("\n");
-      const isExists = usersArray.find((line, idx, arr) => {
-        const [existEmail, correctPassword] = line.split(",")
-        return existEmail.trim() === email && correctPassword.trim() === password;
-      });
+      const usersText = fs.readFileSync("users.txt").toString();
+      const usersArray = usersText.split("\n");
 
-      if (isExists !== undefined) {
-        const userName = isExists.split(",")[2]
-        console.log(`로그인에 성공했어요! 닉네임: ${userName}`);
-      } else {
-        console.log(`이메일이 존재하지 않거나, 잘못된 비밀번호 입니다.`)
-        continue;
+      for (let i = 0; i < usersArray.length; i++) {
+        const [userEmail, userPassword, userNickname] = usersArray[i].split(", ");
+        if (userEmail === email && userPassword === password) {
+          console.log(`로그인에 성공했어요! 닉네임: ${userNickname}`);
+        }
       }
 
+      console.log(`이메일이 존재하지 않거나, 잘못된 비밀번호 입니다.`);
     } else if (answer === "2") {
-      const email = await input(`이메일: `);
+      const email = await input(`이메일을 입력하세요: `);
       if (!email.includes("@")) {
-        console.log(`잘못된 입력입니다. 이메일에 @를 포함시켜주세요.`);
+        console.log(`잘못된 입력입니다. 이메일에 @를 포함시켜주세요.`)
         continue;
       }
 
-      const usersTxt = fs.readFileSync("users.txt").toString();
-      const usersArray = usersTxt.split("\n")
-      const isDuplicate =
-          usersArray.some((line, idx, arr) => {
-            const [savedEmail] = line.split(",")
-            return savedEmail === email;
-            // console.log("element(line): ", line) // 비교하는 배열의 원소 1개
-            // console.log("index(idx): ", idx); // 그 원소의 인덱스
-            // console.log("array(arr): ", arr); // 비교하는 배열 전체
-          });
-
-      if (isDuplicate) {
-        console.log("중복되는 이메일이 있습니다.")
-        continue;
-      }
-
-      // console.log(usersTxt); // txt 파일을 그대로 반환
-      // console.log(usersArray); // txt 파일의 내용을 separator 기준으로 잘라 배열로 반환
-      // console.log(isDuplicate); // 겹치는 이메일이 있으면 true, 그렇지 않으면 false
-
-      const password = await input(`비밀번호: `);
+      const password = await input(`비밀번호를 입력하세요: `);
       if (password.length < 4) {
         console.log(`잘못된 입력입니다. 비밀번호는 4자리이상 입력해주세요.`);
+        continue;
+      }
+
+      const usersText = fs.readFileSync("users.txt").toString();
+      const usersArray = usersText.split("\n")
+
+      let isFoundEmail = false;
+
+      for (let i = 0; i < usersArray.length; i++) {
+        const userEmail = usersArray[i].split(", ")[0];
+        if (userEmail === email) {
+          console.log("중복되는 이메일이 있습니다.");
+          isFoundEmail = true;
+          break;
+        }
+      }
+
+      if (isFoundEmail) {
         continue;
       }
 
       const userName = await input(`닉네임: `);
       fs.appendFileSync("users.txt", `${email}, ${password}, ${userName}\n`);
       console.log(`회원가입에 성공했어요! email: ${email}, password: ${password}, 닉네임: ${userName}`);
-    }
-
-    if (answer !== "1" && answer !== "2") {
+    } else {
       console.log("숫자 1과 2중에서 선택해주십시요.")
-      continue;
     }
   }
 };
